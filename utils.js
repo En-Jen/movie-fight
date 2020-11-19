d53bd78ad1ed8cd0721c54a1393b2af53f1d08fd
Jen Speak <jspeak6@gmail.com>
2020-11-19 15:54:45 -0800

    added dropdown widget

---
+++ utils.js
@@ -0,0 +1,12 @@
+// Wait some amount of time after an event to fetch data
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
